60797e2f726bf4988a0c6ae3831a2cac7ca06780
goodbyebada <goodbyebada@naver.com>
2024-06-03 09:21:38 +0900

    드롭다운 디자인 수정
    select tag -> div ul tag로 구현

--- frontEnd/src/components/containers/formItems/EducationSelector.tsx
+++ frontEnd/src/components/containers/formItems/EducationSelector.tsx
@@ -1,7 +1,5 @@
-// EducationSelector.tsx
-import React from "react";
-import SelectOption from "./SelectOption";
-import { useState } from "react";
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
 
 interface PatronSelectorProps {
   onChange: (value: number) => void;
@@ -13,40 +11,23 @@ const PatronTypeSelector = ({ onChange }: PatronSelectorProps) => {
     { value: "2", label: "대학원" },
   ];
 
-  // const handleGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  //   const selectedGender = e.target.value;
-  //   setGender(selectedGender);
-  //   onChange({ gender: selectedGender }); // 데이터를 상위 컴포넌트로 전달
-  // };
-
   const [patronType, setPatronType] = useState<number>(1);
 
-  const handleGenderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const patronType = parseInt(e.target.value);
-    // 지역변수니까 상관없다
+  const handlePatronTypeChange = (value: string) => {
+    const patronType = parseInt(value);
     setPatronType(patronType);
-    onChange(patronType); // 데이터를 상위 컴포넌트로 전달
+    onChange(patronType);
   };
 
   return (
-    <div className="gender-select">
-      <label htmlFor="genderSelect">학부/대학원 소속 선택</label>
-      <select
-        value={patronType}
-        className="form-select"
-        aria-label="Default select example"
-        onChange={handleGenderChange}
-      >
-        <option value="1">학부생</option>
-        <option value="2">대학원생</option>
-      </select>
+    <div className="patron-select">
+      <label htmlFor="patronTypeSelect">학부/대학원 소속 선택</label>
+      <Dropdown
+        options={patronTypeOptions}
+        selected={patronType.toString()}
+        onChange={handlePatronTypeChange}
+      />
     </div>
-
-    // <SelectOption
-    //   options={patronTypeOptions}
-    //   serverValueMap={{ "1": "학부", "2": "대학원" }}
-    //   serverCallback={handleChange}
-    // />
   );
 };
 
